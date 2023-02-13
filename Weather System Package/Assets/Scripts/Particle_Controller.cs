c1c305066ce932f8b2d5eb4bc34f6d6e73ea2867
NgoziEgonu <71980104+NgoziEgonu@users.noreply.github.com>
2023-02-13 13:58:56 +0000

    Particles

--- Weather System Package/Assets/Scripts/Particle_Controller.cs
+++ Weather System Package/Assets/Scripts/Particle_Controller.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class Particle_Controller : MonoBehaviour
 {
     public ParticleSystem rain;
+    ParticleSystem.MainModule main;
+    public Camera cam;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rain = GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
-        
+        main = rain.main;
+        float camToPlayerDist;
+
+        camToPlayerDist = Vector3.Distance(cam.transform.position, player.transform.position);
+
+        if (camToPlayerDist < 20.0f) //closer to camera is more particles
+        {
+            main.maxParticles = 1000;
+        }
+        else
+        {
+            main.maxParticles = 10;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Distance is: " + camToPlayerDist);
+        }
     }
 }
