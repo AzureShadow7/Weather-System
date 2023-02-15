f4f39105125b8f9537d9c65a73f964614f3a9a76
NgoziEgonu <71980104+NgoziEgonu@users.noreply.github.com>
2023-02-15 16:34:04 +0000

    Rain Particles
    An enum was created to swap between heavy rain and light rain

--- Weather System Package/Assets/Scripts/Particle_Controller.cs
+++ Weather System Package/Assets/Scripts/Particle_Controller.cs
@@ -10,6 +10,14 @@ public class Particle_Controller : MonoBehaviour
     public Camera cam;
     public GameObject player;
 
+    public enum rainIntensity
+    {
+        light,
+        heavy
+    }
+
+    public rainIntensity intensity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,23 +27,36 @@ public class Particle_Controller : MonoBehaviour
     //Update is called once per frame
     void Update()
     {
-        main = rain.main;
-        float camToPlayerDist;
+        //float camToPlayerDist;
 
-        camToPlayerDist = Vector3.Distance(cam.transform.position, player.transform.position);
+        //camToPlayerDist = Vector3.Distance(cam.transform.position, player.transform.position);
 
-        if (camToPlayerDist < 20.0f) //closer to camera is more particles
+        //if (camToPlayerDist < 20.0f) //closer to camera is more particles
+        //{
+        //    main.maxParticles = 1000;
+        //}
+        //else
+        //{
+        //    main.maxParticles = 10;
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    Debug.Log("Distance is: " + camToPlayerDist);
+        //}
+
+        var rainEmRate = rain.emission;
+        switch(intensity)
         {
-            main.maxParticles = 1000;
-        }
-        else
-        {
-            main.maxParticles = 10;
+            case rainIntensity.light:
+                rainEmRate.rateOverTime = 10;
+                break;
+
+            case rainIntensity.heavy:
+                rainEmRate.rateOverTime = 100;
+                break;
         }
 
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log("Distance is: " + camToPlayerDist);
-        }
     }
+
 }
