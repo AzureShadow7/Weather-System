3c83dfd79b6675d99814a90468e4490c4c85ebdf
NgoziEgonu <71980104+NgoziEgonu@users.noreply.github.com>
2023-03-31 04:12:11 +0100

    Rain effect implementation
    The rain's wetness map is now linked to the rain fall particle simulation

--- Weather System Package/Assets/Scripts/Particle_Controller.cs
+++ Weather System Package/Assets/Scripts/Particle_Controller.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 public class Particle_Controller : MonoBehaviour
 {
     private ParticleSystem weatherParticle;
-    //ParticleSystem.MainModule main;
+    private ParticleSystemRenderer weatherParticleRenderer;
+
+    [SerializeField] Material[] particleMaterial;
+
     public Camera cam;
     public GameObject player;
+
+    //private AnimationCurve[] snowSize;
+    AnimationCurve snowSize;
+    AnimationCurve snowSizeMin;
+    AnimationCurve snowSizeMax;
+    public float constantVal;
+
+    //WETNESS
+    [SerializeField] private Material wetnessMat;
 
     public enum weatherParticleIntensity
     {
@@ -23,16 +35,41 @@ public class Particle_Controller : MonoBehaviour
     }
 
     public weatherParticleIntensity intensity;
+    public weatherType weather;
 
     // Start is called before the first frame update
     void Start()
     {
         weatherParticle = GetComponent<ParticleSystem>();
+        weatherParticleRenderer = GetComponent<ParticleSystemRenderer>();
+
+        //snowSize = new AnimationCurve[2];
+
+        //snowSize[0] = new AnimationCurve();
+        //snowSize[0].AddKey(0.0f, 0.5f);
+        //snowSize[0].AddKey(3.0f, 0.45f);
+        //snowSize[0].AddKey(6.0f, 1.0f);
+
+        snowSize = new AnimationCurve();
+        snowSizeMin = new AnimationCurve();
+        snowSizeMax = new AnimationCurve();
+
+        snowSize.AddKey(0.0f, 0.5f);
+        snowSize.AddKey(3.0f, 0.45f);
+        snowSize.AddKey(6.0f, 1.0f);
+
+        snowSizeMin.AddKey(0.0f, 0.5f);
+        snowSizeMax.AddKey(0.0f, 1.0f);
+
+        constantVal = 1.0f;
     }
 
     //Update is called once per frame
     void Update()
     {
+        var weatherParticleEmRate = weatherParticle.emission;
+        var weatherParticleSimSpeed = weatherParticle.main;
+
         //float camToPlayerDist;
 
         //camToPlayerDist = Vector3.Distance(cam.transform.position, player.transform.position);
@@ -51,8 +88,26 @@ public class Particle_Controller : MonoBehaviour
         //    Debug.Log("Distance is: " + camToPlayerDist);
         //}
 
-        var weatherParticleEmRate = weatherParticle.emission;
-        var weatherParticleSimSpeed = weatherParticle.main;
+        switch (weather)
+        {
+            case weatherType.rain:
+                weatherParticleRenderer.material = particleMaterial[0];
+                weatherParticleRenderer.renderMode = ParticleSystemRenderMode.Stretch;
+                weatherParticleSimSpeed.startSize3D = true;
+                weatherParticleSimSpeed.startSizeX = 0.1f;
+                weatherParticleSimSpeed.startSizeY = Random.Range(0.5f, 1.0f);
+                break;
+
+            case weatherType.snow:
+                weatherParticleRenderer.material = particleMaterial[1];
+                weatherParticleRenderer.renderMode = ParticleSystemRenderMode.Billboard;
+                weatherParticleSimSpeed.startSize3D = true;
+                weatherParticleSimSpeed.startSizeX = new ParticleSystem.MinMaxCurve(constantVal, snowSizeMin, snowSizeMax);
+                weatherParticleSimSpeed.startSizeY = new ParticleSystem.MinMaxCurve(constantVal, snowSizeMin, snowSizeMax);
+                break;
+        }
+
+        
 
         //The parameters should be put into a function
         switch(intensity)
@@ -60,11 +115,27 @@ public class Particle_Controller : MonoBehaviour
             case weatherParticleIntensity.light:
                 weatherParticleEmRate.rateOverTime = 10;
                 weatherParticleSimSpeed.simulationSpeed = 1;
+
+                if (weather == weatherType.rain)
+                {
+                    float wetnessAmount = wetnessMat.GetFloat("_Wetness");
+                    wetnessAmount = wetnessAmount += 0.009f * Time.deltaTime;//(Time.time / 50.0f) % 1.2f;
+                    wetnessMat.SetFloat("_Wetness", wetnessAmount);
+                }
+
                 break;
 
             case weatherParticleIntensity.heavy:
                 weatherParticleEmRate.rateOverTime = 100;
                 weatherParticleSimSpeed.simulationSpeed = 3;
+
+                if (weather == weatherType.rain)
+                {
+                    float wetnessAmount = wetnessMat.GetFloat("_Wetness");
+                    wetnessAmount = wetnessAmount += 0.1f * Time.deltaTime;//(Time.time / 50.0f) % 1.2f;
+                    wetnessMat.SetFloat("_Wetness", wetnessAmount);
+                }
+
                 break;
         }
 
