58cdb41225abbf9b8f1544bb3577a974bf245cee
NgoziEgonu <71980104+NgoziEgonu@users.noreply.github.com>
2023-03-31 02:12:55 +0100

    imported package

---
+++ Weather System Package/Assets/Scripts/Particle_Controller.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class Particle_Controller : MonoBehaviour
+{
+    public ParticleSystem rain;
+    //ParticleSystem.MainModule main;
+    public Camera cam;
+    public GameObject player;
+
+    public enum rainIntensity
+    {
+        light,
+        heavy
+    }
+
+    public rainIntensity intensity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rain = GetComponent<ParticleSystem>();
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        //float camToPlayerDist;
+
+        //camToPlayerDist = Vector3.Distance(cam.transform.position, player.transform.position);
+
+        //if (camToPlayerDist < 20.0f) //closer to camera is more particles
+        //{
+        //    main.maxParticles = 1000;
+        //}
+        //else
+        //{
+        //    main.maxParticles = 10;
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    Debug.Log("Distance is: " + camToPlayerDist);
+        //}
+
+        var rainEmRate = rain.emission;
+        var rainSimSpeed = rain.main;
+
+        //The parameters should be put into a function
+        switch(intensity)
+        {
+            case rainIntensity.light:
+                rainEmRate.rateOverTime = 10;
+                rainSimSpeed.simulationSpeed = 1;
+                break;
+
+            case rainIntensity.heavy:
+                rainEmRate.rateOverTime = 100;
+                rainSimSpeed.simulationSpeed = 3;
+                break;
+        }
+
+    }
+}
